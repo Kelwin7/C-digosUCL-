6fb6acde62810f5927da55277e22ba7d3fd3937b
Kelwin7 <kelwindasilvaferreira@gmail.com>
2025-02-24 23:19:08 -0300

    Add files via upload

---
+++ exercicio 6 calculador de raio de circulo.py
@@ -0,0 +1,5 @@
+print ("Bem vindo ao Calculador de Raios de circulos")
+raio = float (input ("digite o Raio:"))
+pi = 3.1416 
+area = pi * ( raio **2)
+print("a aréa do circulo solicitado é", area,"m2")
